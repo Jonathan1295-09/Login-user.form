5b93b0ecab09bb1faf53c640b4d9440077e88a8f
Jonathan1295-09 <jcasasola955@gmail.com>
2024-03-01 23:51:02 -0600

    cookie server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,16 @@ import express from "express"
 import morgan from "morgan"
 import cors from "cors"
 import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
 
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
 
 
 //Routes and routers
