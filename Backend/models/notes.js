3dabc5fc93970335c5df5c6589df81beb29ddc27
Jonathan1295-09 <jcasasola955@gmail.com>
2024-03-03 21:22:10 -0600

    note.js schema

--- Backend/models/notes.js
+++ Backend/models/notes.js
@@ -1,1 +1,13 @@
 import mongoose from "../db/connection.js"
+
+const {Schema, model} = mongoose
+
+const notreSchema = new Schema({
+    title: String,
+    message: String,
+    username: String
+})
+
+const Note = model("Note", notreSchema)
+
+export default Note
