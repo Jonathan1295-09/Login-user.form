9a398b0ec98359160f2cb5c3a4436937c6cdfeb9
Jonathan1295-09 <jcasasola955@gmail.com>
2024-03-10 21:26:15 -0500

    auth signup controller

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -2,3 +2,26 @@ import express from "express"
 import User from "../models/user"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+
+const router = express.Router()
+
+//signup post
+router.post("/signup", async (req, res) => {
+    try{
+    //hash password
+    req.body.password = await bcrypt.hash(req.body.passwor, await bcrypt.genSelt(10))
+    //user
+    const user = await User.create(req.body)
+    //Response
+    res.json({status: "User"})
+    } catch (error){
+        res.status(400).json({error})
+    }
+})
+
+//login post
+router.post("/login", async (req, res) => {
+
+})
+//logout post
+router.post("/logout", async (req, res) => {})
