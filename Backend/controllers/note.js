20d89c1a12cce69d9d9a6c1fdf5e6eae551463d6
Jonathan1295-09 <jcasasola955@gmail.com>
2024-03-30 00:57:01 -0500

    note.js routes

---
+++ Backend/controllers/note.js
@@ -0,0 +1,15 @@
+import express from "express"
+import Note from "../models/notes.js"
+
+const router = express.Router()
+
+////
+router.get("/", async (req,res) => {})
+
+router.get("/", async (req,res) => {})
+
+router.get("/", async (req,res) => {})
+
+router.get("/", async (req,res) => {})
+
+export default router
