549aaca8b8e05e498d931c7a7014528d46c30bad
Jonathan1295-09 <jcasasola955@gmail.com>
2024-03-31 20:10:05 -0500

    note import loggedin.js

--- Backend/controllers/note.js
+++ Backend/controllers/note.js
@@ -1,12 +1,13 @@
 import express from "express"
 import Note from "../models/notes.js"
+import loggedIn from "../utils/loggedIn.js"
 
 const router = express.Router()
 
 ////index////
 router.get("/", async (req,res) => {
     try {
-        
+
     } catch(error){
         res.status(400).json({error})
     }
