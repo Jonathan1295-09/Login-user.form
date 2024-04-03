68f61f138872a7dd83ac2cb877b0c2da2a407b37
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-03 15:58:54 -0500

    route path code

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,3 +1,14 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import App from "../App"
 
+const router = createBrowserRouter(createRoutesFromElements(
+    <>
+    <Route path="/" element = {<App/>}>
+        <Route path="/login" element= {<h1>login</h1>}/>
+        <Route path="/signup" element= {<h1>login</h1>}/>
+        <Route path="/logout" />
+        <Route path="/login" element= {<h1>login</h1>}/>
+        <Route path="/login" element= {<h1>login</h1>}/>
+    </Route>
+    </>
+))
