f00ead3817ceecfe1bb12bd47cf09f9019117293
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-03 16:15:37 -0500

    route.js route path changes

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -10,14 +10,11 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/dashboard" element= {<h1>Dashboard</h1>}>
             <Route path="" element={<h1>index</h1>}/>
             <Route path="/create" element={<h1>Create</h1>}/>
-            <Route path="/:id" element={<h1>Show</h1>}>
-                <Route path="/update" element={<h1>update</h1>}/>
-                <Route path="/delete" element={<h1>delete</h1>}/>
-            </Route>
-            <Route path="/note/:id" element={<h1>Show</h1>}/>
-
-        </Route>
-        <Route path="/login" element= {<h1>login</h1>}/>
+                <Route path="/:id" element={<h1>Show</h1>}>
+                    <Route path="/update" element={<h1>update</h1>}/>
+                    <Route path="/delete" element={<h1>delete</h1>}/>
+                </Route>
+         </Route>
     </Route>
     </>
 ))
