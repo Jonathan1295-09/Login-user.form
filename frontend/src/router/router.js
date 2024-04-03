4e6a41d90c498569fcf4f4cce2122ee3b1b06a2e
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-03 16:30:09 -0500

    router.js route path edit

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -7,14 +7,16 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/login" element= {<h1>login</h1>}/>
         <Route path="/signup" element= {<h1>login</h1>}/>
         <Route path="/logout" />
-        <Route path="/dashboard" element= {<h1>Dashboard</h1>}>
+        <Route path="/dashboard/" element= {<h1>Dashboard</h1>}>
             <Route path="" element={<h1>index</h1>}/>
-            <Route path="/create" element={<h1>Create</h1>}/>
-                <Route path="/:id" element={<h1>Show</h1>}>
-                    <Route path="/update" element={<h1>update</h1>}/>
-                    <Route path="/delete" element={<h1>delete</h1>}/>
+            <Route path="create" element={<h1>Create</h1>}/>
+                <Route path=":id/" element={<h1>Show</h1>}>
+                    <Route path="update" element={<h1>update</h1>}/>
+                    <Route path="delete" element={<h1>delete</h1>}/>
                 </Route>
          </Route>
     </Route>
     </>
 ))
+
+export default router
