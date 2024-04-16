49f1cb6828947616005ce3e972d1f2d347545f49
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-15 19:22:58 -0500

    added button create user

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -4,5 +4,6 @@ export default function Signup(){
     return <Form action="/signup" method="post">
         <input type="text" name="username" placeholder="username"/>
         <input type="password" name="password" placeholder="password"/>
+        <button>Create User</button>
     </Form>
 }
