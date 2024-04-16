77915a3b996760d24b715da729f1e9c023a77b7c
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-15 19:21:53 -0500

    signup function input added

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -3,5 +3,6 @@ import { Form } from "react-router-dom";
 export default function Signup(){
     return <Form action="/signup" method="post">
         <input type="text" name="username" placeholder="username"/>
+        <input type="password" name="password" placeholder="password"/>
     </Form>
 }
