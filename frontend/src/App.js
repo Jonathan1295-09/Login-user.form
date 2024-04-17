b575754192b8e9a653f0b7a7d312b21a46ff9a26
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-17 16:51:11 -0500

    app.js logout changed to form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
      <h1>Scrible</h1>
      <Link to="/signup"><button>signup</button></Link>
      <Link to="/login"><button>login</button></Link>
-     <Link to="/logout"><button>logout</button></Link>
+     <Form action="/logout" method="post"><button>Logout</button></Form>
      <Outlet/>
     </div>
   );
