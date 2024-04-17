c2a4f7cf7efc86c14ab6972fac19f1c035ee71a9
Jonathan1295-09 <jcasasola955@gmail.com>
2024-04-17 16:21:06 -0500

    actions and headers

--- frontend/src/router/actions.js
+++ frontend/src/router/actions.js
@@ -1,2 +1,18 @@
 import url from "./url"
 import { redirect } from "react-router-dom"
+import headers from "./headers"
+
+export const signupAction = async ({request}) => {
+    const formData =  await request.formData()
+
+    const user = {
+        username: formData.get("username"),
+        password: formData.get("password")
+    }
+
+    await fetch(url + "/auth/signup", {
+        method: "post",
+        headers,
+        body: JSON.stringify(user)
+    })
+}
