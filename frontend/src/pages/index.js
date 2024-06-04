9bf676bc93c0c2713274be5607f788f20646afae
Jonathan1295-09 <jcasasola955@gmail.com>
2024-06-03 19:38:14 -0500

    fuction created in index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,12 @@
 import { useRouteLoaderData } from "react-router-dom";
 
 export default function Index(){
-    
+    const notes = useRouteLoaderData()
+    console.log(notes)
+
+    return <div>
+        {notes.map((note) => <div>
+            <h1></h1>
+        </div>)}
+    </div>
 }
