eee2287cdd0e9c6d6abcfed48f5d2fc6cdeb256c
Jonathan1295-09 <jcasasola955@gmail.com>
2024-07-06 11:53:01 -0500

    imported show.js page and showloade as well as connected to the show route

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -4,9 +4,10 @@ import Login from "../pages/login"
 import Signup from "../pages/signup"
 import { loginAction, signupAction,logoutAction, createAction} from "./actions"
 import Dashboard from "../pages/dashboard"
-import { indexLoader } from "./loaders"
+import { indexLoader, showLoader } from "./loaders"
 import Index from "../pages"
 import Create from "../pages/create"
+import Show from "../pages/show"
 
 
 const router = createBrowserRouter(createRoutesFromElements(
@@ -18,7 +19,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/dashboard/" element= {<Dashboard/>}>
             <Route path="" element={<Index/>} loader={indexLoader}/>
             <Route path="create" element={<Create/>} action={createAction}/>
-            <Route path=":id/" element={<h1>Show</h1>}>
+            <Route path=":id/" element={<Show/>} loader={showLoader}>
             <Route path="update" element={<h1>update</h1>}/>
             <Route path="delete" element={<h1>delete</h1>}/>
                 </Route>
