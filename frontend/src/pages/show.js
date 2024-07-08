2f2eef928b15554947fe568602814448c418692b
Jonathan1295-09 <jcasasola955@gmail.com>
2024-07-07 23:00:29 -0500

    show.js update

--- frontend/src/pages/show.js
+++ frontend/src/pages/show.js
@@ -6,6 +6,6 @@ export default function Show(){
         <h2>{note.title}</h2>
         <p>{note.message}</p>
         <Link to={`/dashboard/${note._id}/update`}><button>Update</button></Link>
-        <Form action={'/dashboard/${note._id}/delete'}><button>Delete</button></Form>
+        <Form action={`/dashboard/${note._id}/delete`}><button>Delete</button></Form>
     </section>
 }
